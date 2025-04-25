d5900f77cfec268970a0588bb7b38a145d97dc3f
gra4evp <petrgrachev1@gmail.com>
2025-04-25 23:07:23 +0300

    Backend started

--- services/backend/app/main.py
+++ services/backend/app/main.py
@@ -70,4 +70,4 @@ app.include_router(base_router)
 # Запуск сервер
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
